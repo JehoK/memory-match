eaab06cc534f22d6df07c6d3238ec449e42c0d0a
Jaeho Kim <jehokim90@gmail.com>
2020-06-03 10:36:35 -0700

    match-two-cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,4 +6,34 @@ function handleClick (event) {
     return;
   }
   event.target.className += " hidden";
+
+  if(!firstCardClicked) {
+    firstCardClicked = event.target;
+    firstCardClasses = firstCardClicked.previousElementSibling.className;
+
+  }else {
+    secondCardClicked = event.target;
+    secondCardClasses = secondCardClicked.previousElementSibling.className;
+    gameCards.removeEventListener("click", handleClick);
+    if(firstCardClasses === secondCardClasses) {
+      console.log("match");
+      gameCards.addEventListener("click", handleClick);
+      firstCardClicked = null;
+      secondCardClicked = null;
+    }else {
+      console.log("do not match")
+      setTimeout(function () {
+        firstCardClicked.classList.remove("hidden");
+        secondCardClicked.classList.remove("hidden");
+        firstCardClicked = null;
+        secondCardClicked = null;
+        gameCards.addEventListener("click", handleClick);
+      }, 1500)
+    }
+  }
 }
+
+var firstCardClicked;
+var secondCardClicked;
+var firstCardClasses;
+var secondCardClasses;
